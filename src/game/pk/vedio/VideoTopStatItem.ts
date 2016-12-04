e74f90d3e3038c94affffea1ad5203d4aa7dc44c
hange0o0 <caiweihan@126.com>
2016-12-04 11:42:03 +0800

    1

---
+++ src/game/pk/vedio/VideoTopStatItem.ts
@@ -0,0 +1,42 @@
+
+class VideoTopStatItem extends game.BaseItem {
+    public constructor() {
+        super();
+        this.skinName = "VideoHpItemSkin";
+    }
+
+    private hpText: eui.Label;
+    private backBar: eui.Image;
+    private frontBar: eui.Image;
+
+
+
+    private barWidth = 150;
+    public mcWidth = 160;
+
+    public childrenCreated() {
+        //this.addBtnEvent(this.closeBtn,this.onKill);
+    }
+
+    public dataChanged() {
+        var max = this.data.max;
+        var last = this.data.last;
+        var current = this.data.current;
+        if(this.data.value > 0) {
+            this.hpText.text = '+' + this.data.value
+            this.hpText.textColor = 0x00ff00;
+            this.backBar.source = 'bar1_png'
+            this.backBar.width = current / max * this.barWidth;
+            this.frontBar.source = 'bar3_png'
+            this.frontBar.width = last / max * this.barWidth;
+        }
+        else {
+            this.hpText.text = '' + this.data.value
+            this.hpText.textColor = 0xff0000;
+            this.backBar.source = 'bar2_png'
+            this.backBar.width = last / max * this.barWidth;
+            this.frontBar.source = 'bar3_png'
+            this.frontBar.width = current / max * this.barWidth;
+        }
+    }
+}
